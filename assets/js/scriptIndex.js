79a5b6872c3e69882ce024eaf92561c9f2e53934
Bruno Domingues <p.brunodomingues@gmail.com>
2024-06-11 14:14:23 -0300

    voltando versao

--- assets/js/scriptIndex.js
+++
@@ -1,9 +0,0 @@
-// Função para alternar a visibilidade do menu ao clicar na foto do perfil
-document.getElementById('perfil1').addEventListener('click', function() {
-    var menu = this.querySelector('.menu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
-    }
-});
